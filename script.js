5b76af8b76a414d3fb530db227afad623b6da899
Eduardo <ebarbosa0411@gmail.com>
2025-04-13 15:24:51 -0300

    add checkWinner method

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const gameBoard = (function () {
     // Represent the Tic-Tac-Toe board
     const board = [];
-    const NUMBER_OF_ROWS = 3;
-    const NUMBER_OF_COLUMNS = 3;
+    const BOARD_SIZE = 3;
 
     init();
 
     function init() {
-        for (let i = 0; i < NUMBER_OF_ROWS; i++) {
+        for (let i = 0; i < BOARD_SIZE; i++) {
             const row = [];
-            for (let j = 0; j < NUMBER_OF_COLUMNS; j++) {
-                row.push(makeBoardSpace());
+            for (let j = 0; j < BOARD_SIZE; j++) {
+                row.push(makeBoardSpace(i, j));
             }
             board.push(row);
         }
@@ -39,7 +38,9 @@ const gameBoard = (function () {
     }
 
     function markSpace(mark, row, column) {
-        board[row][column].setMark(mark);
+        const cell = board[row][column]
+        cell.setMark(mark);
+        if (checkWinner(cell)) console.log("GAME WON!");
     }
 
     function reset() {
@@ -51,7 +52,45 @@ const gameBoard = (function () {
     }
 
     function log() {
-        console.log(board);
+        board.forEach((row) => {
+            const arr = [];
+            row.forEach((cell) => {
+                arr.push(cell.getMark());
+            });
+            console.log(arr);
+        });
+    }
+
+    function checkWinner(cell) {
+        // Check row
+        const boardRow = board[cell.getRow()];
+        if (!boardRow.find(checkRow)) return true;
+
+        function checkRow(value) {
+            return value.getMark() != cell.getMark();
+        }
+
+        // Check column
+        const column = cell.getColumn();
+        if (!board.find(checkColumn)) return true;
+
+        function checkColumn(row) {
+            return row[column].getMark() != cell.getMark();
+        }
+
+        // Check diagonal
+        if (!board.find(checkDiagonal)) return true;
+
+        function checkDiagonal(row, index) {
+            return row[index].getMark() != cell.getMark();
+        }
+
+        // Check other diagonal
+        if (!board.find(checkReverseDiagonal)) return true;
+
+        function checkReverseDiagonal(row, index) {
+            return checkDiagonal(row, (BOARD_SIZE - 1 - index));
+        }
     }
 
     return { markSpace, reset, log };
